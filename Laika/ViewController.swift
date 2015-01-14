3178251d38f1c0f8bfe5c346d702ef005b2e6934
Matt Linaberry <matt.linaberry@we-ics.com>
2015-01-14 17:07:03 -0500

    First challenge with Laika complete!

--- Laika/ViewController.swift
+++ Laika/ViewController.swift
@@ -9,15 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var humanYearsTextfield: UITextField!
+    @IBOutlet var dogYearsLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        dogYearsLabel.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func calculateButtonTouched(sender: UIButton) {
+        let humanYearsEntered = humanYearsTextfield.text.toInt()!
+        dogYearsLabel.text = "\(humanYearsEntered) " + "in dog years is " + "\(humanYearsEntered * 7)"
+        dogYearsLabel.hidden = false
+        humanYearsTextfield.resignFirstResponder()
     }
 
 
